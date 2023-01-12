3429f43703f4ef58c3231b33dd750b63782dbdfe
sifislaz <jlazanakis@gmail.com>
2023-01-12 17:49:48 +0200

    conditional rendering approach

--- smartmin_frontend/src/components/NavPanel.vue
+++ smartmin_frontend/src/components/NavPanel.vue
@@ -3,8 +3,9 @@
     <v-list v-model:opened="open" nav>
         <v-chip @click="drawer = !drawer" class="ml-2 mb-4 d-lg-none b-0" color="#bbb" >&lt; Close</v-chip>
         <v-list-item prepend-icon="mdi-home" title="Home" value="home" to="/" class="text-white"></v-list-item>
+        <v-list-item prepend-icon="mdi-login" v-show="!admin" title="Log In" value="login" to="/login" class="text-white"></v-list-item>
         <v-list-item v-show="admin" prepend-icon="mdi-alert-circle" title="Alerts" value="alerts" to="/alerts" class="text-white"></v-list-item>
-        <v-list-group value="Rooms">
+        <v-list-group v-show="admin" value="Rooms">
             <template v-slot:activator="{props}">
                 <v-list-item v-bind="props" prepend-icon="mdi-office-building" title="Rooms" class="text-white"></v-list-item>
             </template>
@@ -25,8 +26,7 @@
     export default {
       data: () => ({
         drawer: false,
-        // admin: false,
-        admin: true,
+        admin: null,
         open:[],
         rooms:[
             {icon:'mdi-desk', title:'Office 1', id:"9bc13"},
@@ -35,6 +35,10 @@
 
 
         ]
- }),
+      }),
+      created(){
+        if(localStorage.getItem('jwt')==null) this.admin = false;
+        else this.admin = true;
+      }
     }
   </script>
