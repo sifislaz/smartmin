99b578de770165ee1dd9cd0a2133317bf0a3624c
sifislaz <jlazanakis@gmail.com>
2023-01-17 19:54:41 +0200

    Created humidity alerting

--- smartmin_backend/controllers/alertController.js
+++ smartmin_backend/controllers/alertController.js
@@ -8,6 +8,7 @@ const checkTempValue = (temp)=>{
     let idealTemp = 22;
     let moderateDeviation = 5;
     let severeDeviation = 15;
+    let message = {};
     if(Math.abs(temp.reading.value - idealTemp) < moderateDeviation){
         return false;
     }
@@ -15,7 +16,7 @@ const checkTempValue = (temp)=>{
         message.severity = 'Medium';
         message.origin = temp.origin.room;
         message.title = `High temperature of ${temp.reading.value} °C, in ${temp.origin.room}.`;
-        message.body = `Sensor ${temo.origin.sensorId} has reported a tempeture of ${temp.reading.value} °C in  ${temp.origin.room}.The heat might be uncomfortable for some.`
+        message.body = `Sensor ${temp.origin.sensorId} has reported a tempeture of ${temp.reading.value} °C in  ${temp.origin.room}.The heat might be uncomfortable for some.`
         createAlert(message);
         //doStuff()
         return true;
@@ -24,7 +25,7 @@ const checkTempValue = (temp)=>{
         message.severity = 'Medium';
         message.origin = temp.origin.room;
         message.title = `Low temperature of ${temp.reading.value} °C, in ${temp.origin.room}.`;
-        message.body = `Sensor ${temo.origin.sensorId} has reported a tempeture of ${temp.reading.value} °C in  ${temp.origin.room}.The cold might be uncomfortable for some.`
+        message.body = `Sensor ${temp.origin.sensorId} has reported a tempeture of ${temp.reading.value} °C in  ${temp.origin.room}.The cold might be uncomfortable for some.`
         createAlert(message);
         //doStuff()
         return true;
@@ -33,7 +34,7 @@ const checkTempValue = (temp)=>{
         message.severity = 'Hazard';
         message.origin = temp.origin.room;
         message.title = `Extremely high temperature of ${temp.reading.value} °C, in ${temp.origin.room}.`;
-        message.body = `Sensor ${temo.origin.sensorId} has reported a tempeture of ${temp.reading.value} °C in  ${temp.origin.room}.The heat might be dangerous for some.`
+        message.body = `Sensor ${temp.origin.sensorId} has reported a tempeture of ${temp.reading.value} °C in  ${temp.origin.room}.The heat might be dangerous for some.`
         createAlert(message);
         //doStuff()
         return true;
@@ -42,7 +43,7 @@ const checkTempValue = (temp)=>{
         message.severity = 'Hazard';
         message.origin = temp.origin.room;
         message.title = `Extremely low temperature of ${temp.reading.value} °C, in ${temp.origin.room}.`;
-        message.body = `Sensor ${temo.origin.sensorId} has reported a tempeture of ${temp.reading.value} °C in  ${temp.origin.room}.The cold might be dangerous for some.`
+        message.body = `Sensor ${temp.origin.sensorId} has reported a tempeture of ${temp.reading.value} °C in  ${temp.origin.room}.The cold might be dangerous for some.`
         createAlert(message);
         //doStuff()
         return true;
@@ -53,23 +54,40 @@ const checkHumValue = (hum)=>{
     let idealRH= 50;
     let moderateDeviation = 10;
     let severeDeviation = 25;
+    let message = {};
     if(Math.abs(hum.reading.value - idealRH) < moderateDeviation){
         return false;
     }
     else if(moderateDeviation<=(hum.reading.value - idealRH)<severeDeviation){  // moderate humidity
-        // createAlert(message);
+        message.severity = "Medium";
+        message.origin = hum.origin.room;
+        message.title = `High humidity of ${hum.reading.value} %, in ${hum.origin.room}`;
+        message.body = `Sensor ${hum.origin.sensorId} has reported a humidity of ${hum.reading.value} % in ${hum.origin.room}.`
+        createAlert(message);
         return true;
     }
-    else if(moderateDeviation<=(idealRH-hum.reading.value)<severeDeviation){  // moderate dryness 
-        // createAlert(message);
+    else if(moderateDeviation<=(idealRH-hum.reading.value)<severeDeviation){  // moderate dryness
+        message.severity = "Medium";
+        message.origin = hum.origin.room;
+        message.title = `Low humidity of ${hum.reading.value} %, in ${hum.origin.room}`;
+        message.body = `Sensor ${hum.origin.sensorId} has reported a humidity of ${hum.reading.value} % in ${hum.origin.room}.`;
+        createAlert(message);
         return true;
     }
     else if((hum.reading.value - idealRH)>= severeDeviation){  // severe humidity
-        // createAlert(message);
+        message.severity = "Hazard";
+        message.origin = hum.origin.room;
+        message.title = `High humidity of ${hum.reading.value} %, in ${hum.origin.room}`;
+        message.body = `Sensor ${hum.origin.sensorId} has reported a humidity of ${hum.reading.value} % in ${hum.origin.room}.`
+        createAlert(message);
         return true;
     }
     else if((idealRH-hum.reading.value) >= severeDeviation){  // severe dryness
-        // createAlert(message);
+        message.severity = "Hazard";
+        message.origin = hum.origin.room;
+        message.title = `Low humidity of ${hum.reading.value} %, in ${hum.origin.room}`;
+        message.body = `Sensor ${hum.origin.sensorId} has reported a humidity of ${hum.reading.value} % in ${hum.origin.room}.`;
+        createAlert(message);
         return true;
     }
 }
