368a4d0a7036175f2188c1f3adeb0a7569d4cdda
Taxis Blounas <118070566+TaxisNet@users.noreply.github.com>
2023-01-18 20:31:39 +0200

    minor cors fix

--- smartmin_backend/middleware/credentials.js
+++ smartmin_backend/middleware/credentials.js
@@ -7,7 +7,7 @@ const credentials = (req, res, next) => {
     const origin = req.headers.origin;
     if (allowedOrigins.includes(origin)) {
         console.log("CORS set");
-        res.header('Access-Control-Allow-Credentials', origin);
+        res.header("Access-Control-Allow-Origin", origin);
     }
     next();
 }
